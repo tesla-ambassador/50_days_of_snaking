cb3ad1f6a69efb8121f0c41f3305dfb5814e064b
Tesla_Ambassador <kevindan641@gmail.com>
2023-07-17 18:15:31 +0300

    checking on my git

--- tesla_ambassador/day_1/extra.py
+++ tesla_ambassador/day_1/extra.py
@@ -1,6 +1,7 @@
 # Write a function called longest_value that takes a dictionary as an argument and returns the first longest value of the dictionary. 
 # For example, the following dictionary should return – apple as the longest value.
 # fruits = {'fruit': 'apple', 'color': 'green'}
+# Random something
 
 fruits = {'fruit': 'apple', 'color': 'green'}
 def longest_value(dic):
